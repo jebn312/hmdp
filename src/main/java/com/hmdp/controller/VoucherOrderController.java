f6deb06dc9a840d2ed0ff7ce2bc27bce80b7f76e
cl <jebn312@163.com>
2025-12-24 20:59:46 +0800

    feat: 全局id生成器、秒杀下单、乐观锁解决超卖问题

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -2,10 +2,15 @@ package com.hmdp.controller;
 
 
 import com.hmdp.dto.Result;
+import com.hmdp.service.IVoucherOrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -15,11 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Api(tags = "秒杀下单接口")
 @RestController
 @RequestMapping("/voucher-order")
 public class VoucherOrderController {
+
+    @Resource
+    private IVoucherOrderService voucherOrderService;
+
+    /**
+     * 秒杀优惠券
+     *
+     * @param voucherId 优惠券id
+     * @return 订单id
+     */
+    @ApiOperation("秒杀优惠下单")
     @PostMapping("seckill/{id}")
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
-        return Result.fail("功能未完成");
+        return voucherOrderService.seckillVoucher(voucherId);
     }
 }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +25,39 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    /**
+     * 秒杀优惠券
+     *
+     * @param voucherId 优惠券id
+     * @return 订单id
+     */
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        //查询优惠券
+        SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
+        if(seckillVoucher == null) {
+            return Result.fail("优惠券不存在");
+        }
+        //判断过期时间
+        if(seckillVoucher.getBeginTime().isAfter(LocalDateTime.now())) return Result.fail("秒杀未开始");
+        if(seckillVoucher.getEndTime().isBefore(LocalDateTime.now())) return Result.fail("秒杀已结束");
+        //判断库存
+        if(seckillVoucher.getStock() < 1) return Result.fail("库存不足");
+        //扣减库存
+        boolean isUpdate = seckillVoucherService.update().setSql("stock = stock - 1").eq("id", voucherId).gt("stock", 0).update();
+        if(!isUpdate) return Result.fail("库存不足");
+        //创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        voucherOrder.setId(redisIdWorker.nextId("order"));
+        voucherOrder.setUserId(UserHolder.getUser().getId());
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        return Result.ok(voucherOrder.getVoucherId());
+    }
 }
